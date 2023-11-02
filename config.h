0f867f4a49fd3566c5eafda919fb5cf97eafad14
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-11-02 00:44:36 -0300

    Implemented config.h and correct socket acceptance

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG
+#define CONFIG
+
+#define PORT 3000
+#define MAX_CONNECTIONS 10
+#define BUFFER 1024
+
+#endif
--- httpd.c
+++ httpd.c
@@ -1,8 +1,75 @@
-#include <stdio.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#if   defined(__OpenBSD__)
+#include <netinet/in.h>
+#endif
+
+#include "config.h"
+
+void error(char * fmt) {
+	fprintf(stdout, "%s\n", fmt);
+	if(errno != 0) {
+		fprintf(stdout, "(errno = %d) : %s\n", errno, strerror(errno));
+	}
+	fflush(stdout);
+	exit(1);
+}
 
 int main() {
-	uint8_t x = 10;
-	printf("%d", x);
+	uint8_t sockfd;
+	uint8_t connfd;
+	uint8_t recvline[BUFFER];
+	uint8_t n;
+
+	struct sockaddr_in serveraddr;
+	struct sockaddr_in clientaddr;
+
+	socklen_t clientaddr_len;
+
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+		error("Failed creating socket.");
+	}
+
+	//TODO: cast of void * 1 needs fix
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *) 1, sizeof(int)) < 0 ) {
+		error("unable to setsockopt");
+	}
+
+	bzero(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_family      = AF_INET;
+	serveraddr.sin_port        = htons(PORT);
+
+	if(bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+		error("Error while binding file descriptor.");
+	}
+
+	if(listen(sockfd, MAX_CONNECTIONS) < 0 ) {
+		error("Listen error.");
+	}
+
+	printf("Listening on: localhost:%d\n", PORT);
+
+	while(1) {
+		connfd = accept(sockfd, (struct sockaddr *) &clientaddr, &clientaddr_len);
+
+		while((n = read(connfd, recvline, BUFFER - 1)) > 0){
+			printf("\n%s\n", recvline);
+			if (recvline[n - 1] == '\n') {
+				break;
+			}
+
+			memset(recvline, 0, BUFFER);
+		}
+		close(connfd);
+	}
+
 	return 0;
 }
